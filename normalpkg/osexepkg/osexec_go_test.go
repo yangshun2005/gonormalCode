38531ca325fd069230db8c7d764013e058a4139f
yangshun <2095686947@qq.com>
2023-03-07 16:56:23 +0800

    add code

--- normalpkg/osexepkg/osexec_go_test.go
+++ normalpkg/osexepkg/osexec_go_test.go
@@ -1,13 +1,16 @@
 package osexepkg
 
-import "testing"
+import (
+	"github.com/yangshun2005/gonormalCode/normalpkg/downloadfile"
+	"testing"
+)
 
 func TestOsexecPkg(t *testing.T) {
 	url := "http://dl.chinaase.com/softwarepackage/k8s/script/t1.sh"
 
-	downloadfile.DownloadFiled(url)
+	DownloadFiled(url)
 
-	fn := ".e/test1.sh"
+	fn := "./test1.sh"
 	OsexecPkgTwo(fn)
 
 }
